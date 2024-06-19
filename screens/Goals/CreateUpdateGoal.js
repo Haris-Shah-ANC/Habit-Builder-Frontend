bb438954c5ea4ed2629bbc04adc54a17c4b65e80
Haris-Shah-ANC <haris.shah@artdexandcognoscis.com>
2024-06-19 20:07:57 +0530

    minor changes in the CreateUpdateGoal.js file

--- screens/Goals/CreateUpdateGoal.js
+++ screens/Goals/CreateUpdateGoal.js
@@ -1,14 +1,15 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import { TextInput } from 'react-native-paper';
 
 const CreateUpdateGoal = () => {
     const [text, setText] = React.useState("");
 
     return (
-        <View style={{paddingTop : 10}}>
+        <View style={{ paddingTop: 10,padding : 10 }}>
+            <Text style={{ paddingLeft: 10 }}>Name Your List</Text>
             <TextInput
-                label="Email"
+                label=""
                 value={text}
                 onChangeText={text => setText(text)}
             />
